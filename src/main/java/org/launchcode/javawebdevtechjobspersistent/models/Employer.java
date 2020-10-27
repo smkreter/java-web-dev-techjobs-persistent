a2807ec8e0ba9cd307c3de8cfd9fa757f1757e20
unknown <smkreter@gmail.com>
2020-10-26 22:53:50 -0500

    i forgot how inheritance works so i need to go watch kyle's video

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,24 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @Size(max = 255)
+    private String location;
 
+    public Employer (String name, String location) {
+//        this.name = name;
+        this.location = location;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
