268f0fd7ab72f619d8264818ece7d690b330edbd
unknown <smkreter@gmail.com>
2020-11-02 19:01:08 -0600

    I added a list of jobs to the employer class with annotations; not sure that I've done the joincolumn annotation correctly

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,13 +1,22 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
 
     @Size(max = 255, message = "The length of the name of the location must be under 255 characters")
     private String location;
+
+    @OneToMany
+    @JoinColumn(name = "employer")
+    private final List<Job> jobs = new ArrayList<>();
+
 
     public Employer (String location) {
 //        this.name = name;
