db734be10bf04a2a5a956f54b5eacd15724adc20
unknown <smkreter@gmail.com>
2020-11-08 11:16:59 -0600

    well i deleted the stuff from the tables now what

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/HomeController.java
@@ -50,7 +50,7 @@ public class HomeController {
     @PostMapping("add")
     public String processAddJobForm(@ModelAttribute @Valid Job newJob,
                                     Errors errors, Model model, @RequestParam int employerId, @RequestParam List<Integer> skills) {
-
+//        @RequestParam int employerId, @RequestParam List<Integer> skills
         if (errors.hasErrors()) {
             model.addAttribute("title", "Add Job");
             model.addAttribute("employers", employerRepository.findAll());
